b3352700e5c623145792e94eaa45902450c2e425
mbaklor <mbaklor@broadcastdesign.co.il>
2023-04-17 00:45:35 +0300

    stole more of prime's config

--- lua/rc/init.lua
+++ lua/rc/init.lua
@@ -2,3 +2,22 @@ require("rc.packer")
 require("rc.set")
 require("rc.remap")
 require("rc.filetypes")
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
