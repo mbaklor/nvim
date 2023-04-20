93ff17b5aa124467e1259f986685a3432fee1d73
mbaklor <mbaklor@gmail.com>
2023-04-20 19:59:07 +0300

    Added window navigation remaps

--- lua/rc/remap.lua
+++ lua/rc/remap.lua
@@ -7,3 +7,20 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
+
+vim.keymap.set("n", "<A-h>","<C-w>h")
+vim.keymap.set("n", "<A-j>","<C-w>j")
+vim.keymap.set("n", "<A-k>","<C-w>k")
+vim.keymap.set("n", "<A-l>","<C-w>l")
+
+vim.keymap.set("n", "<A-+>","<C-w>+")
+vim.keymap.set("n", "<A-->","<C-w>-")
+vim.keymap.set("n", "<A->>","<C-w>>")
+vim.keymap.set("n", "<A-<>","<C-w><")
+
+vim.keymap.set("n", "<A-s>","<C-w>s")
+vim.keymap.set("n", "<A-v>","<C-w>v")
+
+vim.keymap.set("n", "<A-o>","<C-w>o")
