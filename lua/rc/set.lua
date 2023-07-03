857ecf9b383b7c3bd192d3a79a70e4262e676bec
mbaklor <mbaklor@gmail.com>
2023-07-03 12:16:34 +0300

    fixed: shell in windows needed flags

--- lua/rc/set.lua
+++ lua/rc/set.lua
@@ -34,6 +34,8 @@ vim.o.t_Co = 256
 
 if vim.uv.os_uname().sysname == 'Windows_NT' then
     vim.opt.shell = 'pwsh'
+    vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
+    vim.opt.shellxquote = ''
 end
 
 vim.opt.ignorecase = true
