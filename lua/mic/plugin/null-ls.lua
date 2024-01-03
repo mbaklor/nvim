c23d37cd55e05c8b8d51048f1aeac90bd84e0dff
mbaklor <mbaklor@broadcastdesign.co.il>
2024-01-03 19:56:00 +0200

    added python linters

--- lua/mic/plugin/null-ls.lua
+++ lua/mic/plugin/null-ls.lua
@@ -4,5 +4,13 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length","160"}}),
+        null_ls.builtins.diagnostics.pylint.with({extra_args = {
+            "--disable","missing-class-docstring",
+            "--disable","missing-function-docstring",
+            "--disable","missing-module-docstring",
+            "--disable","unspecified-encoding",
+            "--disable","import-error"
+        }})
     }
 })
