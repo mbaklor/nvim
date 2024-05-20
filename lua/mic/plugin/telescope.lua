55f2e8d7bd536791783f9be5dcf9bfe1835405be
mbaklor <mbaklor@gmail.com>
2024-05-20 16:10:36 +0300

    telescope: added picker for files without gitignore

--- lua/mic/plugin/telescope.lua
+++ lua/mic/plugin/telescope.lua
@@ -1,10 +1,16 @@
 local builtin = require('telescope.builtin')
 local utils = require('telescope.utils')
 
+local wrap = function(func, ...)
+    local args = { ... }
+    return function()
+        func(unpack(args))
+    end
+end
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', function()
-    builtin.find_files({ cwd = utils.buffer_dir() })
-end)
+vim.keymap.set('n', '<leader>fi', wrap(builtin.find_files, { no_ignore = true }), {})
+vim.keymap.set('n', '<leader>gf', wrap(builtin.find_files, { cwd = utils.buffer_dir() }), {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
