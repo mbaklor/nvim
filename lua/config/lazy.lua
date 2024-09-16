1e524684320abaae487180059baa11a8a6dbb9d2
mbaklor <mbaklor@gmail.com>
2024-09-16 12:21:00 +0300

    comment string in svelte

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
         { import = "plugins" },
     },
     checker = { enabled = true },
