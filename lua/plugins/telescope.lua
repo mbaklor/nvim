02bb7e8e4d3bfa6c43f54bf137c6a859afe251af
mbaklor <mbaklor@gmail.com>
2024-12-11 16:29:46 +0200

    fix(telescope): better shellslash logic for sveltekit apps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,37 +2,46 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        local sep = '/'
-        local telescopeConfig = require("telescope.config")
-        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        local telescope = require 'telescope'
+        local actions = require 'telescope.actions'
 
-        table.insert(vimgrep_arguments, "--path-separator")
-        table.insert(vimgrep_arguments, "/")
+        -- https://github.com/nvim-telescope/telescope.nvim/issues/2446#issuecomment-2475424902
+        -- https://github.com/MagicDuck/grug-far.nvim/pull/305
+        -- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/a77af2e764c5ed4038d27d1c463fa49cd4794e07/lua/neo-tree/utils/init.lua#L1057
+        local is_windows = vim.fn.has('win64') == 1 or vim.fn.has('win32') == 1
+        local vimfnameescape = vim.fn.fnameescape
+        local winfnameescape = function(path)
+            local escaped_path = vimfnameescape(path)
+            if is_windows then
+                local need_extra_esc = path:find('[%[%]`%$~]')
+                local esc = need_extra_esc and '\\\\' or '\\'
+                escaped_path = escaped_path:gsub('\\[%(%)%^&;]', esc .. '%1')
+                if need_extra_esc then
+                    escaped_path = escaped_path:gsub("\\\\['` ]", '\\%1')
+                end
+            end
+            return escaped_path
+        end
 
+        local select_default = function(prompt_bufnr)
+            vim.fn.fnameescape = winfnameescape
+            local result = actions.select_default(prompt_bufnr, "default")
+            vim.fn.fnameescape = vimfnameescape
+            return result
+        end
 
-        table.insert(vimgrep_arguments, "--glob")
-        table.insert(vimgrep_arguments, "!{.git/*}")
-
-        require('telescope').setup({
+        telescope.setup {
             defaults = {
-                vimgrep_arguments = vimgrep_arguments
-            },
-            pickers = {
-                find_files = {
-                    hidden = true,
-                    find_command = {
-                        "rg",
-                        "--files",
-                        "--color",
-                        "never",
-                        '--glob',
-                        '!{.git/*}',
-                        "--path-separator",
-                        sep,
+                mappings = {
+                    i = {
+                        ['<cr>'] = select_default,
+                    },
+                    n = {
+                        ['<cr>'] = select_default,
                     }
-                }
-            }
-        })
+                },
+            },
+        }
     end,
     init = function()
         local builtin = require('telescope.builtin')
