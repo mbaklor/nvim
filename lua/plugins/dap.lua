b60a895bd7767a36ecb4dae530b5a3e724f84bdd
mbaklor <mbaklor@gmail.com>
2025-04-23 20:03:45 +0300

    dap: terminate the running debug process

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -39,6 +39,7 @@ return {
             vim.keymap.set("n", "<F7>", dap.step_over)
             vim.keymap.set("n", "<F8>", dap.step_out)
             vim.keymap.set("n", "<F9>", dap.step_back)
+            vim.keymap.set("n", "<F10>", dap.terminate)
             vim.keymap.set("n", "<F12>", dap.restart)
 
             dap.listeners.before.attach.dapui_config = function()
