f03d60115107cf9c58f767ada3f522f256f1a727
Michael Baklor <mbaklor@gmail.com>
2025-07-07 14:57:55 +0300

    start markdown with priority I guess

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,10 @@
 return {
-    "OXY2DEV/markview.nvim",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
-    },
+	"OXY2DEV/markview.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+		"saghen/blink.cmp",
+	},
+	lazy = false,
+	priority = 49,
 }
