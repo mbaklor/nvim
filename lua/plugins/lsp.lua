0bebb96e86f1ecb3b1ffbcb8b3b0be1f37e3e674
Michael Baklor <mbaklor@gmail.com>
2026-02-04 14:19:58 +0200

    feat(plugins): removed fidget, similar functionality is built in
    or close enough, I might revert this later

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,9 +23,6 @@ local kickstart = {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-			-- Useful status updates for LSP.
-			{ "j-hui/fidget.nvim", opts = {} },
-
 			-- Allows extra capabilities provided by blink.cmp
 			"saghen/blink.cmp",
 		},
